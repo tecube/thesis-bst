596147818d7e6f79ac5ca3d39a265c89918d8b02
tecube <tecube3@gmail.com>
2023-01-05 23:51:20 +0900

    move test stuff to test/ dir

--- test/test_bib2bibitem.py
+++ test/test_bib2bibitem.py
@@ -6,7 +6,7 @@ def test_inout(tmp_path):
     tmp_output_path = tmp_path.joinpath('test_output.tex')
 
     with open(tmp_output_path, mode='a', encoding='utf-8') as f:
-        formatters = bib2bibitem.parse('test_input.bib')
+        formatters = bib2bibitem.parse('test/test_input.bib')
         bib2bibitem.generate(formatters, f)
 
-    filecmp.cmp(tmp_output_path, 'test_output.tex')
+    filecmp.cmp(tmp_output_path, 'test/test_output.tex')
